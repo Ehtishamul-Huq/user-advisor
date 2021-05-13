37eb59522b3e25cce7abdc8f61de2af40319f6e8
Ehtishamul Huq <ehtishamulhuq7@gmail.com>
2021-05-13 21:33:56 +0530

    first commit

--- advisorapp/serializers.py
+++ advisorapp/serializers.py
@@ -5,7 +5,6 @@ from django.db import models
 from datetime import datetime
 from user.serializers import UserRegistrationSerializer
 from rest_framework.reverse import reverse
-#from .views import AdvisorView, AdvisorBookingView
 
 class AdvisorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,14 +12,12 @@ class AdvisorSerializer(serializers.ModelSerializer):
         fields = ('id','advisor_name','advisor_photo')
 
 class AdvisorBookingSerializer(serializers.ModelSerializer):
-    #profile = AdvisorSerializer(many=True, read_only=True)
+    #profile = AdvisorSerializer(required=False)
     class Meta:
         model = AdvisorBooking
-        fields = ('id', "booking_time")
+        fields = ('id',"booking_time")
+       # extra_kwargs = {'advise':{'read_only':True}}
 
-    #def create(self, validated_data):
-     #   profile = validated_data.pop('profile')
-      #  user = AdvisorSerializer.create(AdvisorSerializer(), validated_data=user_data)
-       # student, created = UnivStudent.objects.update_or_create(user=user,
-        #                    subject_major=validated_data.pop('subject_major'))
-        #return student
+    #def get_object(self, *args, *kwargs):
+
+    
--- advisorapp/views.py
+++ advisorapp/views.py
@@ -1,3 +1,4 @@
+import re
 from .serializers import AdvisorSerializer, AdvisorBookingSerializer
 from user.serializers import UserRegistrationSerializer
 from rest_framework.views import APIView
@@ -27,33 +28,34 @@ class AdvisorView(APIView):
         except User.DoesNotExist:
             return Response({'message':'User does not exist.'}, status=status.HTTP_400_BAD_REQUEST)
 
+
+
+
+######################  DOUBT #############
 class AdvisorBookingView(APIView):
     serializer_class = AdvisorBookingSerializer
-    #lookup_field = 'pk'
-    #lookup_url_kwarg = 'user_pk'
-    def post(self,request, pk, advisor_id):
-        #import pdb;pdb.set_trace()
-        #adbook = Adbook(user=User.objects.get(id=pk), advisor=Advisor.objects.get(id=advisor_id))
-        #queryset = User.objects.get(id=pk)
+    def get_object(self,advisor_id):
+        try:
+            return Advisor.objects.get(id=advisor_id)
+        except Advisor.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request, pk, advisor_id, format=None):
+        #imp/ort pdb;pdb.set_trace()
+        #advisor_id = kwargs.get('advisor_id') 
         #obj = Advisor.objects.get(id=advisor_id)
-        #serializer = AdvisorSerializer(obj)
-        #user_serializer = UserRegistrationSerializer(queryset)
-        #data1 = request.data
-        #_data = {**{"time":request.data}, **{"advisor":pk}}
+        #advise = self.get_object(advisor_id)
+        #_data = {**request.data, **{"advise":advise}}
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-       
-        #response = {**serializer.data, **advisor_serializer.data, **{"advisor_id": advisor_id}}
-        #return Response(response, status=status.HTTP_200_OK)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 class AdvisorDetail(APIView):
     serializer_class = AdvisorBookingSerializer
     def get(self,request,pk):
         #import pdb;pdb.set_trace()
         obj = AdvisorBooking.objects.filter(user=pk)
         serializer = self.serializer_class(obj, many=True)
-        #advisor_id = serializer.data['advisor']
-        #advisor_serializer = AdvisorSerializer(advisor_id)
-
         return Response(serializer.data, status=status.HTTP_200_OK)
