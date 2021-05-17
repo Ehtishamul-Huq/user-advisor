229c4b00a5911b54db7c7f8716dfb98d35fc5530
Ehtishamul Huq <ehtishamulhuq7@gmail.com>
2021-05-17 09:41:27 +0530

    commit

--- advisor/settings.py
+++ advisor/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$&@9)p2h6zsmivwuxv^3-dotfont98+^orvb$4rxcsga%^o*x6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -69,13 +69,13 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'advisor.urls'
--- advisor/urls.py
+++ advisor/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from advisorapp.views import AdvisorAdmin
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/advisor/', AdvisorAdmin.as_view()),
     path('user/', include('user.urls')),
     path('user/', include('advisorapp.urls')),
     path('admin', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
